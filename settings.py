6b9c8e043a7b2142f88588e368c65e5fd3e8b75d
ZZL <zengzhilin99@163.com>
2021-03-05 10:29:21 +0800

    修改mysql配置

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ class Config(object):
     PORT = 3306
     DBNAME = 'meituan'
     # SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}'
-    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://root:123456@127.0.0.1:3306/meituan'
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://root:@zairui123@127.0.0.1:3306/meituan'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     UPLOAD_FOLDER = r'./upload'
